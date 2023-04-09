a989e11ec36a869bc78a58133b9fba5b752131c3
이승언 <dltmddjs1115@gmail.com>
2023-04-09 10:14:29 +0900

    SEO 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,34 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import "../styles/globals.css";
 
+const TITLE = "먼디";
+const DESC = "혼수 비교 견적은 발품 말고 먼디 최저가 구매, 이제 빠르고 간편하게 비교하세요!";
+const URL = "https://mondi.kr";
+const IMAGE = "https://mondi.kr/imgs/og.png";
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>먼디</title>
+        <title>{TITLE}</title>
         <link href="/imgs/favicon.png" rel="icon" type="image/png"/>
         <link href="/imgs/favicon.png" rel="shortcut icon"/>
         <link href="/imgs/favicon.png" rel="apple-touch-icon"/>
+        <meta name="robots" content="index,nofollow" />
+        <meta name="apple-mobile-web-app-title" content={TITLE} />
+        <meta name="description" content={DESC} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESC} />
+        <meta property="og:url" content={URL} />
+        <meta property="og:image" content={IMAGE} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESC} />
+        <meta name="twitter:url" content={URL} />
+        <meta name="twitter:image" content={IMAGE} />
+        <meta name="keywords" content="먼디,mondi,혼수,비교,견젹,발품,최저가" />
       </Head>
       <Component {...pageProps} />
     </>
