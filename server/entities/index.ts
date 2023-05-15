7a84f3c7053a6bd7d6e98b63fffa98ffdd42c972
won-jun-yang <41960405+GAFFWC@users.noreply.github.com>
2023-05-16 00:08:07 +0900

    feat: 스키마 combined export

---
+++ server/entities/index.ts
@@ -0,0 +1,6 @@
+import User from "./user.entity";
+import UserType from "./userType.entity";
+import CommonEntity from "./common";
+import Survey from "./servey.entity";
+
+export const Entities = [User, UserType, CommonEntity, Survey];
