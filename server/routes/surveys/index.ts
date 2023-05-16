9547e04ca2975a813f91aea197efce9a2f75f8d4
won-jun-yang <powerwonjun@naver.com>
2023-05-16 23:43:40 +0900

    feat: survey 컨트롤러 보완

--- server/routes/surveys/index.ts
+++ server/routes/surveys/index.ts
@@ -1,13 +1,17 @@
-import express from "express";
-import next from "next";
-import { getSurveys } from "../../services/surveys/survey.service";
+import express, { Request, Response } from "express";
+import { isEmpty } from "lodash";
+import User from "../../../server/entities/user.entity";
+import { parseUser } from "../../../server/middlewares/user";
+import { getSurvey, getSurveys } from "../../services/surveys/survey.service";
 
 const surveysRouter = express.Router();
 
 surveysRouter
-  .get("/", async (req, res, next) => {
+  .get("/", parseUser, async (req: Request, res: Response, next) => {
     try {
-      const surveys = await getSurveys({});
+      const {page, limit} = req.query as {page: string, limit: string};
+      const user: User = req.auth?.user;
+      const surveys = await getSurveys({userId: user.id, page: parseInt(page) | 1, limit: parseInt(limit) | 10});
       res.json(surveys);
     } catch (err) {
       console.error(err);
@@ -16,7 +20,16 @@ surveysRouter
   })
   .get("/:surveyId", async (req, res, next) => {
     try {
+      const surveyId = parseInt(req.params.surveyId);
+      const user: User = req.auth?.user;
+      const survey = await getSurvey({surveyId, userId: user?.id});
 
+      console.log(survey);
+      if (isEmpty(survey)) {
+        return res.status(404).send();
+      }
+
+      res.json(survey);
     } catch (err) {
       console.error(err);
       next(err);
