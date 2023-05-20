7f113da72401ca14e4db666567ad93f68bbdc752
이승언 <dltmddjs1115@gmail.com>
2023-05-20 12:03:03 +0900

    링크 복사하는 기능 추가

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,19 @@
+import { closeAlert, openAlert } from "./alert";
+
 export const wonComma = (num: number) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const copyUrl = () => {
+  const textarea = document.createElement("textarea");
+  textarea.value = window.location.href;
+  textarea.style.top = "0";
+  textarea.style.left = "0";
+  textarea.style.position = "fixed";
+  document.body.appendChild(textarea);
+  textarea.focus();
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+  openAlert({ title: "링크가 복사되었습니다.", onYes: closeAlert })
+}
