069e896f7c15562d803dce5859e0c25d344739d5
won-jun-yang <41960405+GAFFWC@users.noreply.github.com>
2023-05-22 23:32:01 +0900

    feat: survey dto 타입 추가

--- server/dtos/survey.dto.ts
+++ server/dtos/survey.dto.ts
@@ -1,4 +1,6 @@
-import Survey from "../../server/entities/survey.entity";
+import { pick } from "lodash";
+import User from "../../server/entities/user.entity";
+import Survey, { surveyStates } from "../../server/entities/survey.entity";
 
 export class SurveyDTO extends Survey {
   constructor(survey: Survey) {
@@ -6,5 +8,22 @@ export class SurveyDTO extends Survey {
     Object.assign(this, survey);
   }
   
-  isDone: boolean = false;
+  isEntered: boolean = false;
 };
+
+type SurveyListDTOType = Pick<Survey, "id" | "state" | "brandName" | "itemName" | "itemDescription" | "price" | "targetPrice" | "users">
+export class SurveyListDTO implements SurveyListDTOType {
+  constructor(survey: Survey) {
+    Object.assign(this, pick(survey, ['id', 'state', 'brandName', 'itemName', 'itemDescription', 'price', 'targetPrice', 'users']));
+  }
+
+  id: number;
+  state: surveyStates;
+  brandName: string;
+  itemName: string;
+  itemDescription: string;
+  price: number;
+  targetPrice: number;
+  users: User[];
+  isEntered: boolean = false;
+}
